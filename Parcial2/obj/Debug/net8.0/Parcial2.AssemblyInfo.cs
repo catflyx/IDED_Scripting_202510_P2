f961d04ae0048d7eb5901981df0ba5b0ac472e29
LS1653 <estebanpuerta2006@gmail.com>
2025-09-24 08:08:06 -0500

    todo-terminado

--- Parcial2/obj/Debug/net8.0/Parcial2.AssemblyInfo.cs
+++ Parcial2/obj/Debug/net8.0/Parcial2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Parcial2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3986604bb6bde34b9b0e6500c8f4902a3230e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7853647866de757d01f0cbc9448ae7476513f10")]
 [assembly: System.Reflection.AssemblyProductAttribute("Parcial2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Parcial2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
